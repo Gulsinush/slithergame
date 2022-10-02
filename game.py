b585a752b2998ee92ebbf769236332d8bf0ff06e
Arthur <artursharipv@mail.ru>
2022-10-02 12:38:53 +0500

    add logo

---
+++ game.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import tkinter as tk
+window = tk.Tk()
+window.geometry('600x600')
+window.title("Snake")
+window.mainloop()
+
+
