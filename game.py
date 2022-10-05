9f83683ce3dc834e679f734fd5b70270b670da86
Arthur <artursharipv@mail.ru>
2022-10-05 22:31:24 +0500

    snake move

--- game.py
+++ game.py
@@ -5,14 +5,21 @@ Spyder Editor
 This is a temporary script file.
 """
 import tkinter as tk
+import threading as tm
+import time
+
+basic_coord = ([50,50,70,70],
+               [70,50,90,70],
+               [90,50,110,70],
+               [110,50,130,70]
+               )
 
 class Snake:
     
-    coord_list = []
+    coord_list = list(basic_coord)
     
-    def __init__(self, coord_list):
-        
-        self.coord_list = coord_list
+    def __init__(self):
+        pass
         
 
     def draw(self):
@@ -23,8 +30,15 @@ class Snake:
             canvas.create_rectangle(self.coord_list[i][0], self.coord_list[i][1], 
                                     self.coord_list[i][2], self.coord_list[i][3], 
                                     outline=color, fill="green")
+         
         
-          
+    def change_coord(self):
+        step=5
+        for i in range(len(self.coord_list)):
+            self.coord_list[i][0] -= step
+            self.coord_list[i][2] -= step 
+       
+            
 window = tk.Tk()
 window.geometry('600x600')
 window.title("Snake")
@@ -33,24 +47,19 @@ size = 600
 canvas = tk.Canvas(window, width=size, height=size)
 canvas.pack()
 
-list1 = [[50,50,70,70],
-               [70,50,90,70],
-               [90,50,110,70],
-               [110,50,130,70]
-               ] 
 
-list2 = [[50,90,70,110],
-               [70,90,90,110],
-               [90,90,110,110],
-               [110,90,130,110]
-               ] 
+snake1 = Snake()
+while True:
+    snake1.draw()
+    snake1.change_coord()
+    window.update_idletasks()
+    window.update()
+    time.sleep(0.5)
+    canvas.delete("all")
 
-snake1 = Snake(list1)
-snake2 = Snake(list2)
-snake2.coord_list.append([130,50,150,70])
-snake1.draw()
-snake2.draw()
-window.mainloop()   
+
+
+  
     
 
 
